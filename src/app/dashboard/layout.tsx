1281a8ffc0aa6c076938785b5c08ecdf4e00bcba
FatRicePaddyyyy <for.git.fat.rice.paddy@gmail.com>
2025-01-20 14:29:12 +0900

    サイドバーの追加

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,37 @@
+import { redirect } from "next/navigation";
+
+import { AppSidebar } from "~/components/app-sidebar";
+import { Separator } from "~/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "~/components/ui/sidebar";
+import { auth } from "~/server/auth/auth";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  if (!session) {
+    return redirect("/signin");
+  }
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="min-h-screen bg-cream-four">
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+            </div>
+          </header>
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
